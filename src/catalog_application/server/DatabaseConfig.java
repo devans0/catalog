00649b59fa44fc4c5b8f73125ee23990054fe07c
Dominic Evans <evans.dominic0@proton.me>
2026-01-22 16:07:13 -0700

    Added ConfigLoader and DatabaseConfig classes
    ConfigLoader is responsible for parsing properties files and making
    values associated with desired keys available. Currently only supports
    retrieving String and Integer values.
    
    DatabaseConfig is a static class that is intialized using the
    server.properties file. It generates a connection to the specified
    server via the PostgreSQL JDBC.

--- src/catalog_application/server/DatabaseConfig.java
+++ src/catalog_application/server/DatabaseConfig.java
@@ -1,7 +1,60 @@
+/**
+ * title: Database Configuration
+ * description: Wrapper that encapsulates the configuration of the database server; capable of
+ * 				producing a connection to that server.
+ * @author Dominic Evans
+ * @date January 22, 2026
+ * @version 1.0
+ * @copyright 2026 Dominic Evans
+ */
+
 package catalog_application.server;
 
+import catalog_utils.ConfigLoader;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DatabaseConfig {
+	private static String url;
+	private static String user;
+	private static String pass;
+	private static boolean initialized = false;
 
+	/**
+	 * Creates the static context for the database configuration. Whenever a new DatabaseConfig
+	 * object is created it must be initialized using a properties file before it may be used
+	 * to generate a connection to a database.
+	 * 
+	 * @param propFile containing the values required to create a valid database connection
+	 */
+	public static void initialize(String propFile) {
+		ConfigLoader config = new ConfigLoader(propFile);
+		url = config.getProperty("db.url");
+		user = config.getProperty("db.user");
+		pass = config.getProperty("db.password");
+		initialized = true;
+	} 
+	
+	/**
+	 * Returns a java.sql.Conneciton to the database described by the properties file used to
+	 * intialize the DatabaseConfig object.
+	 * 
+	 * @return java.sql.Connection object to the PostgreSQL server described by the properties file
+	 * @throws SQLException in the case of the PostgreSQL driver not being found or when a connection
+	 * is requested from an uninitialized DatabaseConfig object.
+	 */
+	public static Connection getConnection() throws SQLException {
+		if (!initialized) {
+			throw new SQLException("DatabaseConfig has not been initialized with a properties file.");
+		}
+		try {
+			// Explicitly load the Driver to force an exception if it is not present; this enables
+			// a more descriptive exception to be thrown
+			Class.forName("org.postgresql.Driver");
+			return DriverManager.getConnection(url, user, pass);
+		} catch (ClassNotFoundException cnf) {
+			throw new SQLException("PostgreSQL Driver not found in classpath.", cnf);
+		}
+	}
 }
---
+++ src/catalog_utils/ConfigLoader.java
@@ -0,0 +1,54 @@
+/**
+ * title: Configuration Loader
+ * description: Utility for parsing and returning values from ".properties" files
+ * @author Dominic Evans
+ * @date January 22 2026
+ * @version 1.0
+ * @copyright 2026 Dominic Evans 
+ */
+
+package catalog_utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ConfigLoader {
+	private Properties props = new Properties();
+	
+	/**
+	 * Responsible for loading a properties file so that it may be parsed for values
+	 * associated with different configuration properties.
+	 * 
+	 * @param fileName the name of the file containing properties; suffixed with the
+	 * ".properties" file extension
+	 */
+	public ConfigLoader(String fileName) {
+		try (InputStream input = new FileInputStream(fileName)) {
+			props.load(input);
+		} catch (IOException ioe) {
+			System.err.println("Could not find configuration file: " + fileName);
+			ioe.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Returns the value associated with the provided key
+	 * @param key indicating which property to access
+	 * @return String value associated with the desired property
+	 */
+	public String getProperty(String key) {
+		return props.getProperty(key);
+	}
+	
+	/**
+	 * Fetches the integer value associated with a given key
+	 * @param key indicating which property to access; must be associated with an
+	 * integer-valued property field
+	 * @return Integer value associated with the provided key
+	 */
+	public int getIntProperty(String key) {
+		return Integer.parseInt(props.getProperty(key));
+	}
+}
